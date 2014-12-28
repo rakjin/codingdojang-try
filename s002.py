510ca69f3e8c65b250ec76f6595b3ff66b86a985
grampush <grampush@gmail.com>
2014-12-29 05:14:03 +0900

    trim

--- s002.py
+++ s002.py
@@ -1,5 +1,5 @@
-width = 1
-height = 1
+raw = '6 6'
+width, height = map(int, raw.split(' '))
 
 matrix = [0]*(width*height)
 
@@ -13,19 +13,16 @@ count, index = 0, -1
 
 while go_x >= 0 and go_y >= 0:
   delta = deltas[direction]
-  print('delta: %d' % delta)
   if direction%2 == 0:
     go = go_x
     go_x -= 1
   else:
     go = go_y
     go_y -= 1
-  print('go: %d, go_x: %d, go_y: %d' % (go, go_x, go_y))
   if go > 0:
     for i in range(0, go):
       index += delta
       matrix[index] = count
-      print('matrix[%d] = %d' % (index, count))
       count += 1
   direction = (direction + 1) % 4
 
