5a8ed33230a8eee67d97d5e5e14021ec06812e03
grampush <grampush@gmail.com>
2014-12-29 15:14:27 +0900

    (trivial) (refactoring) wrap into function

--- submit/dq02.py
+++ submit/dq02.py
@@ -4,20 +4,9 @@ import re
 kor_len = len('가') # 2 on win and 3 on osx, ...
 pattern = re.compile('&#\d+;|[가-힣ㄱ-ㅣ]{%d}' % kor_len)
 
-while True:
-  try:
-    raw = raw_input()
-  except (EOFError):
-    break
-  if not raw:
-    break
-
-  raw = raw.split('/')
-  string, ellipsis, desired_len = raw[0], raw[1], int(raw[2])
-  
+def trim_tail(string, ellipsis, desired_len):
   if len(string) <= desired_len:
-    print(string)
-    continue
+    return string
 
   desired_len -= len(ellipsis)
   result = []
@@ -34,4 +23,18 @@ while True:
       string = string[1:]
       count += 1
   result.append(ellipsis)
-  print(''.join(result))
+  return ''.join(result)
+
+if __name__ == '__main__':
+  while True:
+    try:
+      raw = raw_input()
+    except (EOFError):
+      break
+    if not raw:
+      break
+
+    raw = raw.split('/')
+    string, ellipsis, desired_len = raw[0], raw[1], int(raw[2])
+    
+    print(trim_tail(string, ellipsis, desired_len))
