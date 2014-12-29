98a75e1aff748f879be82e1acd3a430c622b99a3
rakjin <grampush@gmail.com>
2014-12-29 22:43:16 +0900

    consider multibyte characters in ellipsis
    - fix bug on case 가나다라마/../10
    - use unicode string

--- dq02.py
+++ dq02.py
@@ -1,37 +1,52 @@
 #-*- coding: utf-8 -*-
 import re
 
-kor_len = len('가') # 2 on win and 3 on osx, ...
-pattern = re.compile('&#\d+;|[가-힣ㄱ-ㅣ]{%d}' % kor_len)
+pattern = re.compile(u'&#\d+;|[가-힣ㄱ-ㅣ]')
+MULTIBYTE_CHARACTER_SIZE = 2
 
-def trim_tail(string, ellipsis, desired_len):
-  if len(string) <= desired_len:
-    return string
-
-  desired_len -= len(ellipsis)
-  result = []
-  count = 0
-  while count < desired_len:
+def len_considering_multibyte_character(string):
+  length = 0
+  while string:
     match = pattern.match(string)
     if match:
-      if count+2 <= desired_len:
+      string = string[match.end():]
+      length += MULTIBYTE_CHARACTER_SIZE
+    else:
+      string = string[1:]
+      length += 1
+  return length
+
+def trim_tail(string, ellipsis, desired_len):
+  if len_considering_multibyte_character(string) <= desired_len:
+    return string
+
+  result = []
+  output_len = 0
+  ellipsis_len = len_considering_multibyte_character(ellipsis)
+  desired_len -= ellipsis_len
+
+  while output_len < desired_len:
+    match = pattern.match(string)
+    if match:
+      if output_len + MULTIBYTE_CHARACTER_SIZE <= desired_len:
         result.append(match.group())
       string = string[match.end():]
-      count += 2
+      output_len += MULTIBYTE_CHARACTER_SIZE
     else:
       result.append(string[:1])
       string = string[1:]
-      count += 1
+      output_len += 1
+  
   result.append(ellipsis)
   return ''.join(result)
 
 if __name__ == '__main__':
   while True:
     try:
-      raw = raw_input()
+      raw = unicode(raw_input(), 'utf8')
       raw = raw.split('/')
       string, ellipsis, desired_len = raw[0], raw[1], int(raw[2])
-    except:
+    except EOFError, e:
       break
     
     print(trim_tail(string, ellipsis, desired_len))
