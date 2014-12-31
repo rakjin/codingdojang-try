e44d74496338db7842cbd39d0e0c7007800abe5b
grampush <grampush@gmail.com>
2014-12-31 13:19:38 +0900

    build data structure

--- dq01.pl
+++ dq01.pl
@@ -1,4 +1,46 @@
 use strict;
 use warnings;
 
-print "Hello, World!";
+my ($height, $width) = split / /, <>;
+
+my $y = 0;
+my @table = ();
+while(<>) {
+	my $x = 0;
+	my @row = ();
+	for my $altitude (split / /) {
+		$altitude =~ s/\s//g;
+		my $node = {
+			"altitude" => $altitude,
+			"adjacents" => [],
+			};
+		push @row, $node;
+		if ($x > 0) {
+			my $left = $row[$x-1];
+			my $left_altitude = $left->{"altitude"};
+			
+			if ($altitude > $left_altitude) {
+				push @{$node->{"adjacents"}}, $left;
+			}
+			elsif ($altitude < $left_altitude) {
+				push @{$left->{"adjacents"}}, $node;
+			}
+		}
+		if ($y > 0) {
+			my $upper = $table[$y-1][$x];
+			my $upper_altitude = $upper->{"altitude"};
+
+			if ($altitude > $upper_altitude) {
+				push @{$node->{"adjacents"}}, $upper;
+			}
+			elsif ($altitude < $upper_altitude) {
+				push @{$upper->{"adjacents"}}, $node;
+			}
+		}
+		$x++;
+	}
+	push @table, \@row;
+	$y++;
+}
+
+
