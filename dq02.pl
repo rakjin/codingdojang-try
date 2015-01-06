54cc7234e8a5301a37c9da2a90eefc4a2a301718
rakjin <grampush@gmail.com>
2015-01-07 05:49:52 +0900

    add tests for 'not trimmed' cases

--- dq02.pl
+++ dq02.pl
@@ -19,6 +19,10 @@ sub length_considering_multibyte_character {
 sub trim_tail {
 	my ($str, $ellipsis, $desired_length) = @_;
 
+	if (length_considering_multibyte_character($str) <= $desired_length) {
+		return $str;
+	}
+
 	return "";
 }
 
@@ -30,6 +34,10 @@ is( length_considering_multibyte_character("&#abcde;"), 8 );
 is( length_considering_multibyte_character("&#;"), 3 );
 is( length_considering_multibyte_character("&#&#12345;;"), 5 );
 is( length_considering_multibyte_character("&#&#12345;;큐"), 7 );
+is( trim_tail("12345", "..", 5), '12345');
+is( trim_tail("한글", "..", 5), '한글');
+is( trim_tail("한글", "..", 4), '한글');
+is( trim_tail("한&#12345;글", "..", 6), '한&#12345;글');
 TODO: {
 	local $TODO = "trim tail";
 	is( trim_tail("1234567890", "..", 5), "123.." );
