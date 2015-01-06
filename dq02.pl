d7301aeb56b7038c38ba1ae840ee4ab8cce76630
rakjin <grampush@gmail.com>
2015-01-07 06:43:30 +0900

    trim considering multibyte chars

--- dq02.pl
+++ dq02.pl
@@ -6,6 +6,8 @@ use warnings;
 
 use Test::More;
 
+use constant MULTIBYTE_CHARACTER_SIZE => 2;
+
 my $pattern_hangul = qr/\p{Hangul}/;
 my $pattern_xmlchar = qr/&#\d+;/;
 my $pattern = qr/$pattern_hangul|$pattern_xmlchar/;
@@ -23,7 +25,28 @@ sub trim_tail {
 		return $str;
 	}
 
-	return "";
+	my @result = ();
+	my $output_length = 0;
+	my $ellipsis_length = length_considering_multibyte_character($ellipsis);
+	$desired_length -= $ellipsis_length;
+
+	while ($output_length < $desired_length) {
+		if ($str =~ s/^($pattern)//) {
+			if ($output_length + MULTIBYTE_CHARACTER_SIZE <= $desired_length) {
+				push @result, $1;
+			}
+			$output_length += MULTIBYTE_CHARACTER_SIZE;
+		}
+		elsif ($str =~ s/^(((?!($pattern)).)+)//) {
+			push @result, substr($1, 0, $desired_length - $output_length);
+			$output_length += length $1;
+		}
+		else {
+			print "\t\tnot expected\n";
+		}
+	}
+	push @result, $ellipsis;
+	return join '', @result;
 }
 
 
@@ -38,8 +61,8 @@ is( trim_tail("12345", "..", 5), '12345');
 is( trim_tail("한글", "..", 5), '한글');
 is( trim_tail("한글", "..", 4), '한글');
 is( trim_tail("한&#12345;글", "..", 6), '한&#12345;글');
-TODO: {
-	local $TODO = "trim tail";
-	is( trim_tail("1234567890", "..", 5), "123.." );
-}
+is( trim_tail("1234567890", "..", 5), "123.." );
+is( trim_tail("한글12345", "..", 5), "한.." );
+is( trim_tail("123한글12345", "..", 7), "123한.." );
+is( trim_tail("123한글12345", "..", 8), "123한.." );
 done_testing();
