1b13c40420646e878402738131eb660dd2ee5ccc
rakjin <grampush@gmail.com>
2015-01-07 05:13:47 +0900

    pass a test for counting a hangul as 2 length long

--- dq02.pl
+++ dq02.pl
@@ -1,20 +1,21 @@
 #!/usr/bin/perl
 
+use utf8;
 use strict;
 use warnings;
 
 use Test::More;
 
+my $pattern = qr/\p{Hangul}/;
+
 sub length_considering_multibyte_character {
 	my $str = shift;
+	$str =~ s/$pattern/AA/g;
 	return length $str;
 }
 
 
 
 is( length_considering_multibyte_character("world"), 5 );
-TODO: {
-	local $TODO = 'considering length of a hangul char as 2';
-	is( length_considering_multibyte_character("한글"), 4 );
-}
+is( length_considering_multibyte_character("한글"), 4 );
 done_testing();
